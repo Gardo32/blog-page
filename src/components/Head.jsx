75a6c2be2b269cf6de791c03b3096d549b874186
Gardo32 <gardodaqqaq@gmail.com>
2024-02-28 07:01:03 +0000

    Refactor navigation menu in Head component

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
 import { images } from '../constants'
 import { IoMdMenu, IoMdClose } from "react-icons/io";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 
 const navItemInfo = [
-  {name: 'Home'},
-  {name: 'Articles'},
-  {name: 'Pages'},
-  {name: 'Pricing'},
-  {name: 'Faq'},
+  {name: 'Home', type: "link" },
+  {name: 'Articles', type: "link" },
+  {name: 'Pages', type: "dropdown", items: ['About us , Contact us'] },
+  {name: 'Pricing', type: "link" },
+  {name: 'Faq', type: "link" },
 ]
 
-const NavItem = ({name}) => {
+const NavItem = ({item}) => {
   return(
     <li className=' relative group'>
-      <a href='/' className='px-4 py-2 '>
-        {name}
+    {item.type === 'link' ? <>
+    <a href='/' className='px-4 py-2 '>
+      {item.name}
+    </a>
+    <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
+      /</span> </> : 
+      <>
+       <a href='/' className='px-4 py-2'> 
+        {item.name}
       </a>
-      <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>/</span>
-    </li>
-  )
+      <div className='hidden trans'>
+
+      </div>
+      </>
+      }
+  </li>
+   )
 }
 
 const Head = () => {
@@ -38,16 +50,16 @@ const naVvisibility = () => {
         <div>
           <img className='w-16' src={images.logo} alt='logo' />
         </div>
-        <div>
+        <div className='z-50 lg:hidden'>
           {navIsVisible ? <IoMdClose onClick={naVvisibility} className='w-6 h-6' /> : <IoMdMenu onClick={naVvisibility} className='w-6 h-6' />}
         </div>
-        <div className={`${navIsVisible ? 'right-0' : 'right-full'} flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 -right-full lg:static flex gap-x-9 items-center`}>
-          <ul className='flex gap-x-2 font-semibold'>
+        <div className={`${navIsVisible ? 'right-0' : 'right-full'} transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 -right-full lg:static flex gap-x-9 items-center`}>
+          <ul className=' text-white items-center gap-y-6 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold'>
              {navItemInfo.map((item) => (
-              <NavItem key={item.name} name={item.name} />
+              <NavItem key={item.name} item={item} />
             ))}
           </ul>
-          <button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300' >
+          <button className='mt-6 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300' >
             Sign in
             </button>
         </div>
