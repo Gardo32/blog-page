4de115bb3ff2f7885b4ab1360ce1162ba51e24ee
Gardo32 <gardodaqqaq@gmail.com>
2024-03-03 08:13:36 +0000

    Refactor dropdown menu in Head component

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -13,30 +13,30 @@ const navItemInfo = [
 ]
 
 const NavItem = ({item}) => {
-  const [dropdownIsVisible, setDropdownIsVisible] = React.useState(false)
-  const dropdownVisibility = () => {
-    setDropdownIsVisible((curState) => {
-      return !curState
-    })
-  }
   return(
     <li className=' relative group'>
     {item.type === 'link' ? <>
-    <button href='/' className='px-4 py-2' onClick={dropdownVisibility}>
+    <a href='/' className='px-4 py-2 '>
       {item.name}
-    </button>
-    <span className=' cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
+    </a>
+    <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
       /</span> </> 
       : 
-      <div className={`${dropdownIsVisible ? "block" : "hidden"} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}>
+      <>
+       <a href='/' className='px-4 py-2'> 
+        <span>{item.name}</span>
+        <IoMdArrowDropdown className='inline-block' />
+      </a>
+      <div className='hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max'>
         <ul className='flex flex-col shadow-lg rounded-lg overflow-hidden'>
           {item.items.map((page) => (
-            <a href='/' className='px-4 py-2 hover:bg-dark-hard hover:text-white text-white lg:text-dark-hard transition-all duration-300'>
-              {page}
-            </a>
+              <a href='/' className='px-4 py-2 hover:bg-dark-hard hover:text-white text-white lg:text-dark-hard transition-all duration-300'>
+                {page}
+              </a>
           ))}
         </ul>
       </div>
+      </>
       }
   </li>
    )
