3678a1d8148420ad089ef5dac4460a663a51c57c
Gardo32 <gardodaqqaq@gmail.com>
2024-03-06 06:49:28 +0000

    Add CTA component and update Article layout (part 7)

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,11 +2,13 @@ import logo from '../assets/Logo.svg';
 import HeroImage from '../assets/HeroImage.svg';
 import laptop from '../assets/posts/ales-nesetril-Im7lZjxeLhg-unsplash 1.jpg';
 import profilepic from '../assets/posts/griffin-wooldridge-7Sz71zuuW4k-unsplash 1.png';
+import CtaImage from '../assets/posts/alexander-shatov-CTZhGbSxWLI-unsplash 1.png';
 const images = {
     logo,
     HeroImage,
     laptop,
     profilepic,
+    CtaImage,
 };
 
 export default images;
--- src/pages/home/container/Article.jsx
+++ src/pages/home/container/Article.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { FaArrowRight } from "react-icons/fa";
 import ArticileCard from '../../../components/ArticileCard'
 
 const Article = () => {
   return (
-    <section className='container mx-auto flex flex-wrap md:gap-x-5 gap-y-5 px-5 py-10'>
-        <ArticileCard className='w-full md:w-[48%] lg:w-[32%]' />
-        <ArticileCard className='w-full md:w-[48%] lg:w-[32%]' />
+    <section className='flex flex-col container mx-auto px-5 py-10'>
+      <div className='flex flex-wrap md:gap-x-5 gap-y-5 pb-10'>
+        <ArticileCard className='w-full md:w-[50%-20px] lg:w-[32%]' />
+        <ArticileCard className='w-full md:w-[50%-20px] lg:w-[32%]' />
+      </div>
+      <button className='mx-auto flex  items-center gap-x-2 fond-bold text-primary border-2 border-primary px-6 py-3 rounded-lg'>
+        <span>
+           More Articles
+        </span>
+        <FaArrowRight className='w-3 h-3' />
+      </button>
     </section>
   )
 }
