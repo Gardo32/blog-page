cdbd87dd3a58a7d48a5ccf384ed148471d862635
Gardo32 <gardodaqqaq@gmail.com>
2024-03-07 18:02:05 +0000

    Refactor BreadCrumbs and ArticleDetailPage components, add tags to SuggestedPosts (part 5 final)

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -27,39 +27,47 @@ const postsData = [
     _id: 1,
     image: images.laptop,
     title: 'Help children get better education',
-    createdAt: "2024-03-7T00:00:00Z",
+    createdAt: Date.now(),
   },
   {
     _id: 2,
     image: images.laptop,
     title: 'Help children get better education',
-    createdAt: "2024-03-7T00:00:00Z",
+    createdAt: Date.now(),
   },
   {
     _id: 3,
     image: images.laptop,
     title: 'Help children get better education',
-    createdAt: "2024-03-7T00:00:00Z",
+    createdAt: Date.now(),
   },
   {
     _id: 4,
     image: images.laptop,
     title: 'Help children get better education',
-    createdAt: "2024-03-7T00:00:00Z",
+    createdAt: Date.now(),
   },
-  ]
-
+  ];
+const tagsData = [
+  "Medical",
+  "Lifestyle",
+  "Learn",
+  "Health",
+  "Diet",
+  "Education",
+  "Food",
+]
 const ArticleDetailPage = () => {
   return (
     <MainLayout>
-        <section className='container mx-auto mx-w-5xl flex flex-col px-5 py-5'>
+        <section className='container mx-auto mx-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start'>
              <article className='flex-1'>
               <BreadCrumbs data={breadCrumbsData} />
               <img className='rounded-xl w-full' src={images.laptop} alt="laptop" />
-              <Link to="/blog?category=selectedCategory" className="text-primary text-sm font-roboto inline-block mt-4">
+              <Link to="/blog?category=selectedCategory" className="text-primary text-sm font-roboto inline-block mt-4 md:text-base">
                 EDUCATION
               </Link>
-              <h1 className='text-xl font-medium font-roboto text-dark-hard'>
+              <h1 className='md:text-[26px] text-xl font-medium font-roboto text-dark-hard'>
                 Help children get better education
               </h1>
               <div className='mt-4 text-dark-soft'>
@@ -68,7 +76,11 @@ const ArticleDetailPage = () => {
                 </p>
               </div>
              </article>
-             <SuggestedPosts header="Latest Article" posts={postsData}/>
+             <SuggestedPosts header="Latest Article" 
+             posts={postsData} 
+             tags={tagsData}
+             className="mt-8 lg:mt-0 max-w-xs"
+              />
         </section>
     </MainLayout>
   )
--- src/pages/articleDetail/container/SuggestedPosts.jsx
+++ src/pages/articleDetail/container/SuggestedPosts.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { images } from "../../../constants";
+import { BsTags } from "react-icons/bs";
 
 const SuggestedPosts = ({ className, header, posts = [], tags }) => {
 return (
     <div
         className={`w-full shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] rounded-lg p-4 ${className}`}
     >
-        <h2 className="font-roboto font-medium text-dark-hard md:text-xl">
+        <h2 className=" font-roboto font-medium text-dark-hard md:text-xl">
             {header}
         </h2>
         <div className="grid gap-y-5 mt-5 md:grid-cols-2 md:gap-x-5 lg:grid-cols-1">
@@ -16,14 +17,28 @@ return (
                     className="flex space-x-3 flex-nowrap items-center">
                         <img src={item.image} alt="image" className="aspect-square object-cover rounded-lg w-1/5" />
                         <div>
-                            <h3 className="text-sm font-roboto text-dark-hard font-medium">
+                            <h3 className="md:text-base text-sm font-roboto text-dark-hard font-medium">
                                 {item.title}
                             </h3>
-                            <span>
-                                {new Date(item.createdAt).toLocaleString("en-US")}
+                            <span className="text-xs opacity-60">
+                            {new Date(item.createdAt).toLocaleDateString("en-US", {
+                                day: "numeric",
+                                month: "short",
+                                year: "numeric",
+                                })}
                             </span>
                         </div>
                     </div>
+                ))}
+            </div>
+            <h2 className="font-roboto font-medium text-dark-hard mt-8 md:text-xl">
+                Tags
+            </h2>
+            <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
+                {tags.map((item) => (
+                    <Link to="/" className="inline- rounded-md px-3 py-1.5 bg-primary font-roboto text-xs text-white md:text-sm">
+                        {item}
+                    </Link>
                 ))}
             </div>
     </div>
