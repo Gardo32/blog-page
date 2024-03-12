0b61ae0529e583c79bb3313607882b43e934b70b
Gardo32 <gardodaqqaq@gmail.com>
2024-03-12 20:28:34 +0000

    vid 10 final

--- src/components/comments/CommentsContainer.jsx
+++ src/components/comments/CommentsContainer.jsx
@@ -3,58 +3,85 @@ import { getCommentsData } from '../../data/comments';
 import CommentForm from './CommentForm';
 import Comment from './Comment';
 
-    const CommentsContainer = ({className, loginedUserId}) => {
-      const [comments, setComments] = useState([]);
-      const mainComments = comments.filter((comment) => !comment.parent);
-      const [affectedComment, setaffectedComment] = useState(null);
+const CommentsContainer = ({className, loginedUserId}) => {
+  const [comments, setComments] = useState([]);
+  const mainComments = comments.filter((comment) => !comment.parent);
+  const [affectedComment, setaffectedComment] = useState(null);
 
-      console.log(comments);
+  console.log(comments);
 
-      useEffect(() => {
-        (async () => {
-          const commentData = await getCommentsData();
-          setComments(commentData);
-        })();
-      }, []);
+  useEffect(() => {
+    (async () => {
+      const commentData = await getCommentsData();
+      setComments(commentData);
+    })();
+  }, []);
 
-    const addCommentsHandler = (value, parent = null, replyOnUser = null) => {
-        const newComment = {
-            _id: "10",
-            user: {
-              _id: "a",
-              name: "Mohammad Rezaii",
-            },
-            desc: value,
-            post: "1",
-            parent: parent,
-            replyOnUser: replyOnUser,
-            createdAt: "2022-12-31T17:22:05.092+0000",
-        };
-        setComments((curState) => { 
-          return [newComment, ...curState];
-        })
-
+  const addCommentsHandler = (value, parent = null, replyOnUser = null) => {
+    const newComment = {
+      _id: Math.random().toString(),
+      user: {
+        _id: "a",
+        name: "Mohammad Rezaii",
+      },
+      desc: value,
+      post: "1",
+      parent: parent,
+      replyOnUser: replyOnUser,
+      createdAt: new Date().toISOString(),
     };
+    setComments((curState) => { 
+      return [newComment, ...curState];
+    });
+  };
+
+  const updateCommentsHandler = (value, commentId) => {
+    const updatedComments = comments.map((comment) => {
+      if (comment._id === commentId) {
+        return {
+          ...comment,
+          desc: value,
+        };
+      }
+  const deleteCommentsHandler = (commentId) => {
+    const updatedComments = comments.filter((comment) => comment._id !== commentId);
+    setComments(updatedComments);
+  }
+      return comment;
+    });
+    setComments(updatedComments);
+    setaffectedComment(null);
+  };
+
+  const getRepliesHandler = (commentId) => {
+    return comments.filter((comment) => comment.parent === commentId)
+    .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    );
+  }
+
   return (
     <div className={`${className}`}>
-        <CommentForm 
+      <CommentForm 
         btnLabel="Send" 
         formSubmitHanlder={(value) => addCommentsHandler(value)} 
-        />
-        <div className='space-y-4 mt-8'>
-          {mainComments.map((comment) => () => (
-            <Comment 
+      />
+      <div className='space-y-4 mt-8'>
+        {mainComments.map((comment) => () => (
+          <Comment 
             key={comment._id}
             comment={comment} 
             logginedUserId={loginedUserId} 
             affectedComment={affectedComment} 
             setAffectedComment={setaffectedComment}
             addComment={addCommentsHandler}
-            />
-          ))}
-        </div>
+            updateComment={updateCommentsHandler} 
+            deleteComment={deleteCommentsHandler}
+            replies={getRepliesHandler(comment._id)}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CommentsContainer
+export default CommentsContainer;
